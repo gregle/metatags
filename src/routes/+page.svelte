e8e34d11dd8a2fd028503bd6359c63ec42d806c3
DESKTOP-68K65QB\Justin <justin@jcrastelli.com>
2025-08-12 23:48:43 -0700

    updating the hom page info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,13 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 <section>
-	<h1>Fun w/ meta tags</h1>
-
+	<h1>Fun w/ Meta Tags</h1>
 	<p>
 		I set up this little app to help test and verify how sites consume the various ways we can
 		define meta data on a site. Particularly when link previews are being generated. Each page uses
 		the meta data in their &lt;head&gt; elements that is displayed in the associated code snippets.
-		This lets developers have a better idea of how best to represent their websites and projects.
-		Each page also has a special twitter specific message when the associated links are shared on
-		that sesspool of a site.
+		This lets me quickly compare types when posting to various platforms and have a better idea of
+		how best to represent my websites and projects.
 	</p>
 </section>
 
