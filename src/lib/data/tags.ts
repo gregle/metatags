61bc529f4fa89393df74fc1cb7f95a38f4d2f913
DESKTOP-68K65QB\Justin <justin@jcrastelli.com>
2025-08-12 22:29:49 -0700

    fixing the urls

--- src/lib/data/tags.ts
+++ src/lib/data/tags.ts
@@ -26,6 +26,7 @@ export const tags: {
     certain apps cut off the message at different points.
     Surely the app has cut off by now right? "/>
 `,
+		tabText: 'Description',
 		title: 'Basic HTML - Title & Description',
 		text: "The most basic implementation, but this time with a description. There's no technical limitation to how long your description can be, but most previews cut off after a couple of sentences. And rightfully so, folks should be clicking into the page if they're that invested right?"
 	},
@@ -34,7 +35,7 @@ export const tags: {
 		title: 'Dynamic Title Demo',
 		text: ''
 	},
-	'OG:Basics': {
+	'OG-Basics': {
 		code: `
   <meta charset="utf-8" />
   <title>OG:Basic</title>
@@ -45,10 +46,11 @@ export const tags: {
   <meta property="og:description" content="Getting started with Open Graph definitions.
     Defining a type, a title, and a description." />
 `,
+		headerTitle: 'OG:Basic',
 		title: 'Open Graph - Basics',
 		text: "The Open Graph protocol is a set of standards originally annouced by Facebook in 2010 which define metadata tags that describe content and controls how it's displayed when sharing links on social media. It has been widely adopted by many platforms at this point. In the above example it duplicates the definition for title and definition, so when posted to most social media apps, the OG tags will be preferenced, but the title the browser tab will preference the standard html title tag. If the OG:description tag is omitted, most sites fallback to the standard description meta tag. This cascading definitions can be useful when targetting specific information to social media apps.<br/>In order for your content to be represented with the og:type tag. There are full type veritcals for music amd video with subtypes (eg: albums, movie, apisode), along with other simplier types like article, profile, or in this case, website."
 	},
-	'OG:Image': {
+	'OG-Image': {
 		code: `
   <meta charset="utf-8" />
   <title>OG:Image</title>
@@ -65,10 +67,11 @@ export const tags: {
   <meta property="og:image:height" content="600" />
   <meta property="og:image:alt" content="A preview screenshot of my personal portfolio" />
 `,
+		headerTitle: 'OG:Image',
 		title: 'Open Graph - Image',
 		text: "One of the most common OG tags you'll see is the preview image. This gives a potential user some idea of what to expect when they click. Some good strategies are either a screenshot of the page, or a logo/image that represents the content well. Recommended resolution is 2:1 ratio for large cards and 1:1 for small card with a minimum dimension of 300x157. You can also explicitly define additional structured data like height and width with an extra :. Put structured content immediately after it's root tag, both for better organization, and because most parsers will consider it done as soon as the next root element is encountered. Some platforms even provide support for gifs as preview images! Though not all of them, so make sure that first frame is meaningful as that will likely be the fallback still."
 	},
-	'OG:Video': {
+	'OG-Video': {
 		code: `
   <meta charset="utf-8" />
   <title>OG:Video</title>
@@ -84,10 +87,11 @@ export const tags: {
   <meta property="og:video:width" content="720" />
   <meta property="og:video:height" content="720" />
 `,
+		headerTitle: 'OG:Video',
 		title: 'Open Graph - Video',
 		text: 'The structured data for the video tag matches the image tags fairly closely (kind of like if videos were just a series of images or something). Keep in mind though, showing your content like this, may prevent the need for a user to click through to your page, instead favoring to watch directly on whatever third part platform the link has been posted to.'
 	},
-	'OG:Audio': {
+	'OG-Audio': {
 		code: `
   <meta charset="utf-8" />
   <title>OG:Audio</title>
@@ -100,6 +104,7 @@ export const tags: {
   <meta property="og:audio:secure_url" content="https://assets.gregle.dev/projects/metatags/JOHN-CENA.mp3" />
   <meta property="og:audio:type" content="audio/mpeg" />
 `,
+		headerTitle: 'OG:Audio',
 		title: 'Open Graph - Audio',
 		text: "The audio tag however has fewer sub-properties, as sounds don't have a height or width. But depending on how the recieving platform has implemented things, it also allows for users to consume the content directly from the link preview."
 	},
@@ -122,6 +127,7 @@ export const tags: {
     }
   </script>
 `,
+		headerTitle: 'Schema.org',
 		title: 'Schema.org - Search Engines',
 		text: "Schema is another standard for describing content on the web created by Google, Microsoft, Yahoo and Yandex to make the job of their search web scrappers easier. It's documentation is long and complicated and honestly, after years of them making Google Search worse, I don't think it's my job to make it better, and likely defining these schemas just make it easier for their AI to hoover up your content right before they charge you for the privledge of using it again. So better to let Google and friends worry about their own bots and save your time for building cool things. But this is how you make those fancy google results your marketing department keeps going on about."
 	},
